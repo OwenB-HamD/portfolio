8923b4c680e70d986086dc3dce13c69fe5f877bd
OwenB-HamD <owen.b@hamiltoniandynamics.com>
2022-07-18 17:01:41 +0100

    home page built, not finished

---
+++ website/src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <p className="intro"> Hello, my name is</p>
+            <h1 className="name">Owen Bovill</h1>
+            <h2 className="title">I am a full-stack web developer</h2>
+        </div>
+    )
+}
